aeb61619dd7a63150f8a2e84835e33a775df2f8a
horizonnns <for.office.gthb@gmail.com>
2025-10-31 22:47:57 +0500

    Add yookassa dependency and integrate payments router into the Express app
    Update package.json to include the yookassa library for payment processing. Modify index.ts to incorporate a new payments router, enhancing the API with payment-related functionality

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,16 @@
 import express from "express";
+import paymentsRouter from "./routes/payments";
 import { router as rewriteRouter } from "./routes/rewrite";
+
 import { initScheduler } from "./scheduler/resetUsage";
 import dotenv from "dotenv";
-dotenv.config();
 
+dotenv.config();
 const app = express();
+
 app.use(express.json());
 app.use("/api", rewriteRouter);
+app.use("/api/payments", paymentsRouter);
 
 initScheduler(); // ← запускаем планировщик
 
