536cf96b0340533ab0e3ba9951015d7f5eda7a58
horizonnns <for.office.gthb@gmail.com>
2025-11-16 20:02:36 +0500

    Add initial migration files for database structure
    - Introduced migration_lock.toml to manage migration state.
    - Created migration.sql to establish tables for User, OfferMessage, Referral, and Payment, enhancing the database schema for improved functionality and organization

---
+++ src/db/migrations/20251116150215_init/migration.sql
@@ -0,0 +1,50 @@
+-- CreateTable
+CREATE TABLE "User" (
+    "id" SERIAL NOT NULL,
+    "telegramId" TEXT,
+    "dailyLimit" INTEGER NOT NULL DEFAULT 5,
+    "lastUsedAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "isPremium" BOOLEAN NOT NULL DEFAULT false,
+    "premiumUntil" TIMESTAMP(3),
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "OfferMessage" (
+    "id" SERIAL NOT NULL,
+    "telegramId" TEXT NOT NULL,
+    "messageId" INTEGER NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "OfferMessage_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Referral" (
+    "id" SERIAL NOT NULL,
+    "inviterId" TEXT NOT NULL,
+    "invitedId" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "Referral_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Payment" (
+    "id" SERIAL NOT NULL,
+    "telegramId" TEXT NOT NULL,
+    "paymentId" TEXT NOT NULL,
+    "amount" DOUBLE PRECISION NOT NULL,
+    "currency" TEXT NOT NULL,
+    "status" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "Payment_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_telegramId_key" ON "User"("telegramId");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Payment_paymentId_key" ON "Payment"("paymentId");
