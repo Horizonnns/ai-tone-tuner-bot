d63834bef199cdb2ec5284ec2b027863460b45f5
horizonnns <for.office.gthb@gmail.com>
2025-11-16 19:46:57 +0500

    Remove old migration for OfferMessage table and add new migrations for User, OfferMessage, Referral, and Payment tables, enhancing database structure and functionality

--- src/db/migrations/20251111101106_init/migration.sql
+++
@@ -1,9 +0,0 @@
--- CreateTable
-CREATE TABLE "OfferMessage" (
-    "id" SERIAL NOT NULL,
-    "telegramId" TEXT NOT NULL,
-    "messageId" INTEGER NOT NULL,
-    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
-
-    CONSTRAINT "OfferMessage_pkey" PRIMARY KEY ("id")
-);
--- src/db/migrations/20251116144518_init/migration.sql
+++ src/db/migrations/20251116144518_init/migration.sql
@@ -11,6 +11,16 @@ CREATE TABLE "User" (
 );
 
 -- CreateTable
+CREATE TABLE "OfferMessage" (
+    "id" SERIAL NOT NULL,
+    "telegramId" TEXT NOT NULL,
+    "messageId" INTEGER NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "OfferMessage_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
 CREATE TABLE "Referral" (
     "id" SERIAL NOT NULL,
     "inviterId" TEXT NOT NULL,
